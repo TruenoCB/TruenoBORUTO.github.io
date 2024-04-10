9aecf1b4c43fadc029fab67526f990e99ae21486
trueno <2877164746@qq.com>
2024-04-10 10:07:14 +0800

    Site updated: 2024-04-10 10:07:14

--- js/matery.js
+++ js/matery.js
@@ -138,8 +138,8 @@ $(function () {
     function showOrHideNavBg(position) {
         let showPosition = 100;
         if (position < showPosition) {
-            $nav.addClass('nav-transparent');
             $nav.removeClass('nav-transparent2');
+            $nav.addClass('nav-transparent');
             $backTop.slideUp(300);
         } else {
             $nav.removeClass('nav-transparent');
